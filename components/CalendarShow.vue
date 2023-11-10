1fd1a55341e96722c6b30e2c55e409e752a9bcf4
Pasha <vasilievpavel795@gmail.com>
2023-11-11 01:33:19 +0300

    update CalendarShow

--- components/CalendarShow.vue
+++ components/CalendarShow.vue
@@ -1,33 +1,40 @@
 <template>
-  <div class="flex flex-col items-center">
-    <div class="text-center mb-2">
-      <span v-if="visibleMonths.length === 1">{{ visibleMonths[0] }}</span>
-      <span v-else>{{ visibleMonths.join(" - ") }}</span>
-    </div>
-    <div class="flex items-center justify-between">
-      <button
-        @click="scroll(-1)"
-        :disabled="isPrevDisabled"
-        class="p-2 disabled:opacity-50"
-      >
-        <span>&lt;</span>
-      </button>
-      <div class="flex overflow-hidden">
-        <div
-          v-for="date in visibleDates"
-          :key="date.toString()"
-          class="flex-none w-12 h-12 bg-gray-700 shadow-md mx-1 flex items-center justify-center rounded"
+  <div class="flex flex-col items-center m-5">
+    <div class="flex w-full justify-between items-center mb-2">
+      <div class="flex-grow text-left font-bold">
+        <span v-if="visibleMonths.length === 1">{{ visibleMonths[0] }}</span>
+        <span v-else>{{ visibleMonths.join(" - ") }}</span>
+      </div>
+      <div class="flex-grow-0">
+        <button
+          @click="scroll(-1)"
+          :disabled="isPrevDisabled"
+          class="p-2 disabled:opacity-50"
         >
+          <span>&lt;</span>
+        </button>
+        <button
+          @click="scroll(1)"
+          :disabled="isNextDisabled"
+          class="p-2 disabled:opacity-50"
+        >
+          <span>&gt;</span>
+        </button>
+      </div>
+    </div>
+    <div class="flex justify-center">
+      <div
+        v-for="date in visibleDates"
+        :key="date.toString()"
+        class="flex flex-col items-center mx-1 bg-gray-700 rounded-lg w-9 px-4 py-1"
+      >
+        <div class="text-white text-xs">
+          {{ date.toLocaleDateString("ru-RU", { weekday: "short" }) }}
+        </div>
+        <div class="bg-gray-700 flex items-center justify-center h-6">
           {{ date.getDate() }}
         </div>
       </div>
-      <button
-        @click="scroll(1)"
-        :disabled="isNextDisabled"
-        class="p-2 disabled:opacity-50"
-      >
-        <span>&gt;</span>
-      </button>
     </div>
   </div>
 </template>
@@ -60,7 +67,7 @@ const visibleDates = computed(() => {
 
 const visibleMonths = computed(() => {
   const months = visibleDates.value.map((date) =>
-    date.toLocaleString("default", { month: "long" })
+    date.toLocaleString("ru-RU", { month: "long" })
   );
   return [...new Set(months)];
 });
@@ -78,7 +85,7 @@ function scroll(direction) {
       allDates.value
         .slice(startIndex.value, endIndex.value)
         .map((date) =>
-          date.toLocaleString("default", { month: "long", year: "numeric" })
+          date.toLocaleString("ru-RU", { month: "long", year: "numeric" })
         )
     ),
   ];
