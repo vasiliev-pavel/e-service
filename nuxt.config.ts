f0703a4894327f9f4c8ae7769da0f5a6eb25ff0e
Pasha <vasilievpavel795@gmail.com>
2023-11-21 19:38:27 +0300

    update url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
   supabase: {
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_KEY,
-    // redirect: true,
+    redirect: true,
     // не удалять может пригодится в будушем
     redirectOptions: {
       login: "/login",
