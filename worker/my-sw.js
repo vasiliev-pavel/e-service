9613be88327a808677f6ab17cd5f40e82c2d291d
Pasha <vasilievpavel795@gmail.com>
2023-11-29 18:31:34 +0300

    update sw2

--- worker/my-sw.js
+++ worker/my-sw.js
@@ -1,5 +1,25 @@
 // Service Worker
+import { clientsClaim } from "workbox-core";
+import {
+  precacheAndRoute,
+  cleanupOutdatedCaches,
+  createHandlerBoundToURL,
+} from "workbox-precaching";
+import { registerRoute, NavigationRoute } from "workbox-routing";
 
+self.skipWaiting();
+clientsClaim();
+precacheAndRoute(self.__WB_MANIFEST);
+cleanupOutdatedCaches();
+
+//You can remove this code if you aren't precaching anything, or leave it in and live with the warning message
+// try {
+//   const handler = createHandlerBoundToURL("/");
+//   const route = new NavigationRoute(handler);
+//   registerRoute(route);
+// } catch (error) {
+//   console.warn("Error while registering cache route", { error });
+// }
 self.addEventListener("push", (event) => {
   let data = {};
   // Проверка наличия данных в push-событии
