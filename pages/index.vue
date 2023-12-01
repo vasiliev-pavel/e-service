54f857dfa756f31b7661845f4cd3fa9a1d629aad
Pasha <vasilievpavel795@gmail.com>
2023-12-01 12:54:14 +0300

    update sw on index page

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,13 @@
     Sign out
   </button>
   <button
-    @click="sendNotification"
+    @click="requestNotificationPermission"
+    class="w-full bg-green-500 text-white p-3 rounded-lg font-semibold text-lg block text-center"
+  >
+    Enable Notifications
+  </button>
+  <button
+    @click="sendUserNotification"
     class="w-full bg-blue-200 text-white p-3 rounded-lg font-semibold text-lg block text-center"
   >
     send notification
@@ -24,67 +30,63 @@ const businessStore = useBusinessStore();
 const { data } = await useFetch("/api/business");
 const VAPID_PUBLIC_KEY =
   "BO7QziEK_cUB-ZBrx5aSfcwidid6FOQckMWdnbTA6XFAnZzl-KGF3IBI1hviD7qmX1Hw-822wDAZ_Cm35x5ZylY";
+const notificationPermission = ref(Notification.permission);
 
-onMounted(async () => {
-  if ("serviceWorker" in navigator && "PushManager" in window) {
-    const serviceWorkerRegistration = await navigator.serviceWorker.register(
-      "./sw.js"
-    );
-    console.info("Service worker was registered.", serviceWorkerRegistration);
-
-    const result = await Notification.requestPermission();
-    if (result === "denied") {
-      console.error("The user explicitly denied the permission request.");
-      return;
-    }
-    if (result === "granted") {
-      console.info("The user accepted the permission request.");
-    }
-    console.log("tyt0");
-    const registration = await navigator.serviceWorker.getRegistration();
-    if (!registration) {
-      console.error("Service Worker registration not found.");
-      return;
-    }
-    console.log("tyt1");
-    const subscribed = await registration.pushManager.getSubscription();
-    if (subscribed) {
-      console.info("User is already subscribed.");
-    }
-    console.log("tyt2");
-
-    const subscription = await registration.pushManager.subscribe({
-      userVisibleOnly: true,
-      applicationServerKey: urlB64ToUint8Array(VAPID_PUBLIC_KEY),
-    });
-
-    console.log("tyt3");
-    console.log("Subscription:", subscription);
-
-    if (user.value) {
-      await $fetch("/api/notification/addSubscription", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: [
-          {
-            user_id: user.value.id,
-            endpoint: subscription,
-          },
-        ],
-      });
-    }
-    // Обновление состояния кнопок
-  } else {
-    console.error("Browser does not support service workers or push messages.");
-  }
-});
-
-watchEffect(() => {
+watchEffect(async () => {
   if (user.value) {
     if (data.value) {
       businessStore.setBusiness(data.value);
+    }
+  }
+  if (notificationPermission.value === "granted") {
+    if ("serviceWorker" in navigator && "PushManager" in window) {
+      try {
+        const serviceWorkerRegistration =
+          await navigator.serviceWorker.register("./sw.js");
+        console.info(
+          "Service worker was registered.",
+          serviceWorkerRegistration
+        );
+
+        const registration = await navigator.serviceWorker.getRegistration();
+        if (!registration) {
+          console.error("Service Worker registration not found.");
+          return;
+        }
+
+        const subscribed = await registration.pushManager.getSubscription();
+        if (subscribed) {
+          console.info("User is already subscribed.");
+        } else {
+          const subscription = await registration.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: urlB64ToUint8Array(VAPID_PUBLIC_KEY),
+          });
+
+          console.log("Subscription:", subscription);
+
+          if (user.value) {
+            await $fetch("/api/notification/addSubscription", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: [
+                {
+                  user_id: user.value.id,
+                  endpoint: subscription,
+                },
+              ],
+            });
+          }
+        }
+      } catch (error) {
+        console.error("Error during service worker registration:", error);
+      }
+    } else {
+      console.error(
+        "Browser does not support service workers or push messages."
+      );
     }
   }
 });
@@ -94,37 +96,60 @@ const logout = async () => {
   navigateTo("/login");
 };
 
-const sendNotification = async () => {
-  if (user.value) {
-    const { data: pushSubscription } = await useFetch(
+const sendUserNotification = async () => {
+  if (!user.value) return;
+
+  try {
+    const { data: subscriptionData } = await useFetch(
       `/api/notification/getSubscriptions/${user.value.id}`
     );
-    if (!pushSubscription.value) {
-      console.error("No data received from fetch");
-      return;
+
+    if (!subscriptionData.value) {
+      throw new Error("No subscription data available");
     }
-    const data = pushSubscription.value;
-    const temp = JSON.parse(data.data[0].endpoint);
-    // await useFetch(`/api/notification/sendNotification/${user.value.id}`);
-    const tempData = {
-      endpoint: temp.endpoint,
+
+    const subscriptionDetails = JSON.parse(
+      subscriptionData.value.data[0].endpoint
+    );
+
+    const notificationPayload = {
+      endpoint: subscriptionDetails.endpoint,
       keys: {
-        p256dh: temp.keys.p256dh,
-        auth: temp.keys.auth,
+        p256dh: subscriptionDetails.keys.p256dh,
+        auth: subscriptionDetails.keys.auth,
       },
     };
-    const registration = await navigator.serviceWorker.getRegistration();
-    if (registration) {
-      const subscription = await registration.pushManager.getSubscription();
-      if (subscription && user.value) {
-        console.log(subscription);
 
-        await $fetch("/api/notification/sendNotification", {
-          method: "POST",
-          body: tempData,
-        });
-      }
-    }
+    const serviceWorkerRegistration =
+      await navigator.serviceWorker.getRegistration();
+    if (!serviceWorkerRegistration)
+      throw new Error("Service Worker registration not found");
+
+    const activeSubscription =
+      await serviceWorkerRegistration.pushManager.getSubscription();
+    if (!activeSubscription) throw new Error("No active subscription found");
+
+    console.log(activeSubscription);
+    await $fetch("/api/notification/sendNotification", {
+      method: "POST",
+      body: notificationPayload,
+    });
+  } catch (error) {
+    console.error("Notification Error:", error);
+  }
+};
+
+const requestNotificationPermission = async () => {
+  if (!("Notification" in window)) {
+    console.error("This browser does not support desktop notification");
+    return;
+  }
+
+  try {
+    const result = await Notification.requestPermission();
+    notificationPermission.value = result;
+  } catch (error) {
+    console.error("Error requesting notification permission:", error);
   }
 };
 </script>
