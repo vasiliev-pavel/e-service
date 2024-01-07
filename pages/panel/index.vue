8ca931f83693725baf14edddddd6c895cbbb4a32
Gizat Uteev <gizat.uteev@gmail.com>
2024-01-07 14:13:18 -0600

    add business fixed

--- pages/panel/index.vue
+++ pages/panel/index.vue
@@ -5,7 +5,6 @@
     <UButton class="text-lg" to="/panel/appointments">Appointments</UButton>
     <UButton class="text-lg" to="/panel/business">Business</UButton>
     <UButton class="text-lg" to="/panel/categories">Categories</UButton>
-    <UButton class="text-lg" to="/panel/companies">Companies</UButton>
     <UButton class="text-lg" to="/panel/services">Services</UButton>
     <UButton class="text-lg" to="/panel/specialists">Specialists</UButton>
   </div>
